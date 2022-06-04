7fe9468629c5a9b5bdda0d3c4a3eb546809a970b
Omar Iraqi <o.iraqi@gmail.com>
2022-06-04 19:29:21 +0100

    updates

--- a3quality/models/course_ilo_so.py
+++ a3quality/models/course_ilo_so.py
@@ -6,7 +6,7 @@ class CourseIlOSO(models.Model):
     _description = 'Course ILO & SO Mapping'
     _sql_constraints = [('a3quality_ilo_so_ukey', 'unique(course_ilo_id, so_id)', 'ILO/SO mapping already exists')]
 
-    course_ilo_id = fields.Many2one(comodel_name='a3catalog.course.ilo', string='ILO', required=True)
+    course_ilo_ids = fields.Many2many(comodel_name='a3catalog.course.ilo', string='ILOs')
     so_id = fields.Many2one(comodel_name='a3quality.student.outcome', string='SO', required=True)
     course_program_id = fields.Many2one(comodel_name='a3quality.course.program', string='Course/Program', required=True)    
     level = fields.Selection(string='Level', selection=[('introduce', 'Introduce'), ('reinforce', 'Reinforce'), ('emphasize', 'Emphasize')], default='introduce', required=True)
