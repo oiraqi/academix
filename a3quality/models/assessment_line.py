4b0d5215d173ff627738a6a2a6b8e4c009a975e5
Omar Iraqi <o.iraqi@gmail.com>
2022-06-05 23:31:13 +0100

    updates

--- a3quality/models/assessment_line.py
+++ a3quality/models/assessment_line.py
@@ -1,3 +1,26 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Al Akhawayn University in Ifrane -- AUI
+#    Copyright (C) 2022-TODAY AUI(<http://www.aui.ma>).
+#
+#    Author: Omar Iraqi Houssaini | https://github.com/oiraqi
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
 from odoo import api, models, fields
 from odoo.exceptions import UserError
 
@@ -5,6 +28,7 @@ from odoo.exceptions import UserError
 class AssessmentLine(models.Model):
 	_name = 'a3quality.assessment.line'
 	_description = 'AssessmentLine'
+	_sql_constraints = [('assessment_ilo_ukey', 'unique(assessment_id, ilo_id)', 'Duplicate ILO assessment')]
 
 	assessment_id = fields.Many2one('a3quality.assessment', 'Assessment', required=True)
 	course_id = fields.Many2one(comodel_name='a3.course', related='assessment_id.course_id')
--- a3quality/models/portfolio.py
+++ a3quality/models/portfolio.py
@@ -1,3 +1,26 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Al Akhawayn University in Ifrane -- AUI
+#    Copyright (C) 2022-TODAY AUI(<http://www.aui.ma>).
+#
+#    Author: Omar Iraqi Houssaini | https://github.com/oiraqi
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
 from odoo import api, models, fields
 
 
@@ -5,6 +28,7 @@ class Portfolio(models.Model):
 	_name = 'a3quality.portfolio'
 	_description = 'Portfolio'
 	_inherit = 'a3.faculty.activity'
+	_sql_constraints = [('section_ukey', 'unique(section_id)', 'A portfolio already exists for this section')]
 
 	name = fields.Char('Name', compute='_compute_name', store=True)
 	section_id = fields.Many2one('a3roster.section', 'Section', required=True)
