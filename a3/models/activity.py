4246bed9bc44583929ce9685252cedfe37f6d841
Omar Iraqi <o.iraqi@gmail.com>
2022-06-08 18:50:44 +0100

    updates

--- a3/models/activity.py
+++ a3/models/activity.py
@@ -45,12 +45,9 @@ class Activity(models.AbstractModel):
     year = fields.Selection(_year_selection, 'Year', required=True, default=lambda self: str(date.today().year - 2001) + '/' + str(date.today().year - 2000) if date.today().month < 8 else str(date.today().year - 2000) + '/' + str(date.today().year - 1999))
     semester = fields.Selection(
         [('1', 'Fall'), ('2', 'Spring'), ('3', 'Summer')], 'Semester', default=lambda self: '1' if date.today().month >= 8 and date.today().month <= 12 else '2' if date.today().month >= 1 and date.today().month <= 5 else '3', required=True)
-    term = fields.Char(string='Term', compute='_onchange_semester_or_year')
+    term_id = fields.Many2one('a3.term', string='Term', compute='_onchange_semester_or_year', store=True)
     suffix = fields.Char(compute='_onchange_semester_or_year')
     prefix = fields.Char(compute='_onchange_semester_or_year')
-    byear = fields.Integer(compute='_onchange_semester_or_year', store=True)
-    bsemester = fields.Selection(
-        [('1', 'Spring'), ('2', 'Summer'), ('3', 'Fall')], 'Semester', compute='_onchange_semester_or_year', store=True)
 
     document_ids = fields.One2many(
         'ir.attachment', string='Documents', compute='_document_ids')
@@ -73,20 +70,14 @@ class Activity(models.AbstractModel):
         for rec in self:
             year = int(rec.year.split('/')[0]) + 2000
             if rec.semester == '1':
-                rec.term = 'Fall - ' + str(year)
+                rec.term_id = self.env['a3.term'].sudo().get_or_create(year, '3')
                 rec.suffix = '-FA' + str(year - 2000)
-                rec.prefix = 'FA' + str(year - 2000) + '-'
-                rec.byear = year
-                rec.bsemester = '3'
+                rec.prefix = 'FA' + str(year - 2000) + '-'                
             elif rec.semester == '2':
-                rec.term = 'Spring ' + str(year + 1)
+                rec.term_id = self.env['a3.term'].sudo().get_or_create(year + 1, '1')
                 rec.suffix = '-SP' + str(year - 1999)
                 rec.prefix = 'SP' + str(year - 1999) + '-'
-                rec.byear = year + 1
-                rec.bsemester = '1'
             else:
-                rec.term = str(year + 1) + ' - Summer'
+                rec.term_id = self.env['a3.term'].sudo().get_or_create(year + 1, '2')
                 rec.suffix = '-SU' + str(year - 1999)
                 rec.prefix = 'SU' + str(year - 1999) + '-'
-                rec.byear = year + 1
-                rec.bsemester = '2'
--- a3/models/term.py
+++ a3/models/term.py
@@ -21,11 +21,32 @@
 #
 ###############################################################################
 
+from a3.models.activity import SEMESTERS
 from odoo import api, fields, models
 
+SEMESTERS = {'1': 'Spring', '2': 'Summer', '3': 'Fall'}
 
 class Term(models.Model):
     _name = 'a3.term'
     _description = 'Academic Term'
+    _order = 'year,semester'
+    _sql_constraints = [('term_ukey', 'unique(year, semester)', 'Term already exists')]
 
-    name = fields.Char(string='Name')
+    @api.model
+    def get_or_create(self, year, semester):
+        records = self.search([('year', '=', year), ('semester', '=', semester)])
+        if records:
+            return records[0]
+        return self.create({'year': year, 'semester': semester})
+
+    name = fields.Char(string='Term', compute='_compute_name', store=True)
+    year = fields.Integer(string='Year', required=True)
+    semester = fields.Selection(
+        [('1', 'Spring'), ('2', 'Summer'), ('3', 'Fall')], 'Semester', required=True)
+
+    @api.depends('year', 'semester')
+    def _compute_name(self):
+        for rec in self:
+            if rec.year and rec.semester:
+                rec.name = SEMESTERS[rec.semester] + ' ' + str(rec.year)
+    
