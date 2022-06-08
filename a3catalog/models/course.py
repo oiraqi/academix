a4adbf52cea2ea95bfa6266585bcdc16fb03134c
Omar Iraqi <o.iraqi@gmail.com>
2022-06-08 19:26:14 +0100

    updates

--- a3catalog/models/course.py
+++ a3catalog/models/course.py
@@ -55,10 +55,7 @@ class Course(models.Model):
         for rec in self:
             corequisites = self.env['a3catalog.corequisite'].search(
                 [('course_id', '=', rec.id)])
-            if not corequisites:
-                rec.corequisite_ids = False
-            else:            
-                rec.corequisite_ids = [corequisite.corequisite_id.id for corequisite in corequisites]            
+            rec.corequisite_ids = [corequisite.corequisite_id.id for corequisite in corequisites]            
 
     def _corequisite_for_ids(self):
         for rec in self:
