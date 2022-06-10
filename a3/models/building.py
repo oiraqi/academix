e8dbde02075a05d745a888f4f97c25e0e0b7b426
Omar Iraqi <o.iraqi@gmail.com>
2022-06-10 18:33:50 +0100

    updates

--- a3/models/building.py
+++ a3/models/building.py
@@ -27,6 +27,7 @@ from odoo import fields, models
 class Building(models.Model):
     _name = 'a3.building'
     _description = 'Building'
+    _sql_constraints = [('b=name_ukey', 'unique(name)', 'Building already exists')]
 
     name = fields.Char(string='Name', required=True)
     room_ids = fields.One2many(comodel_name='a3.room', inverse_name='building_id', string='Rooms')
