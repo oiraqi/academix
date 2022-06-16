7b8ed62efa5f120bfec0592d1c2a83b9d1db2dc9
Omar Iraqi <o.iraqi@gmail.com>
2022-06-17 00:25:29 +0100

    updates

--- a3quality/models/course_ilo_so.py
+++ a3quality/models/course_ilo_so.py
@@ -27,9 +27,9 @@ from odoo import api, fields, models
 class CourseIloSO(models.Model):
     _name = 'a3quality.course.ilo.so'
     _description = 'Course ILO & SO Mapping'
-    _sql_constraints = [('a3quality_ilo_so_course_program_so_ukey', 'unique(course_program_id, so_id)', 'SO mapping already exists')]
+    _sql_constraints = [('a3quality_ilo_so_course_program_so_ukey', 'unique(course_program_id, ilo_id, so_id)', 'Mapping already exists')]
 
-    ilo_ids = fields.Many2many(comodel_name='a3catalog.course.ilo', string='ILOs')
+    ilo_id = fields.Many2one(comodel_name='a3catalog.course.ilo', string='ILO', required=True)
     so_id = fields.Many2one(comodel_name='a3quality.student.outcome', string='SO', required=True)
     course_program_id = fields.Many2one(comodel_name='a3quality.course.program', string='Course/Program', required=True)    
     level = fields.Selection(string='Level', selection=[('introduce', 'Introduce'), ('reinforce', 'Reinforce'), ('emphasize', 'Emphasize')], default='introduce', required=True)
--- a3quality/models/course_program.py
+++ a3quality/models/course_program.py
@@ -31,7 +31,7 @@ class CourseProgram(models.Model):
 
     course_id = fields.Many2one(comodel_name='a3.course', string='Course', required=True)
     program_id = fields.Many2one(comodel_name='a3catalog.program', string='Program', required=True)
-    ilo_so_ids = fields.One2many(comodel_name='a3quality.course.ilo.so', inverse_name='course_program_id', string='Mapping')
+    ilo_so_ids = fields.One2many(comodel_name='a3quality.course.ilo.so', inverse_name='course_program_id', string='ILO/SO Mapping')
     discipline_id = fields.Many2one(comodel_name='a3.discipline', related='course_id.discipline_id', store=True)
     school_id = fields.Many2one(comodel_name='a3.school', related='course_id.school_id', store=True)
     ilo_ids = fields.One2many('a3catalog.course.ilo', related='course_id.ilo_ids', string="Course ILOs")
