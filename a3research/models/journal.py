28f376473869fe87a0e436d160de2e89b1770eeb
Omar Iraqi <o.iraqi@gmail.com>
2022-06-16 10:27:42 +0100

    updates

--- a3research/models/journal.py
+++ a3research/models/journal.py
@@ -3,9 +3,23 @@ from odoo import models, fields
 
 class Journal(models.Model):
 	_name = 'a3research.journal'
+	_inherit = 'mail.thread'
 	_description = 'Journal'
 
 	name = fields.Char('Name', required=True)
+	publisher_id = fields.Many2one(comodel_name='a3research.publisher', string='Publisher', required=True)
+	editor = fields.Char('Editor(s)')	
 	impact = fields.Selection(
         [('if', 'Impact Factor'), ('ix', 'Indexed'), ('nix', 'Non-indexed')], 'Impact', required=True)
+	tier = fields.Selection(string='Tier', selection=[('1', '1'), ('2', '2'), ('3', '3')], required=True, tracking=True)
+	state = fields.Selection(string='State', selection=[('proposed', 'Proposed'),
+		('approved', 'Approved'), ('rejected', 'Rejected')], default='proposed', required=True, tracking=True)
+	
+	def approve(self):
+		self.ensure_one()
+		self.state = 'approved'
+
+	def reject(self):
+		self.ensure_one()
+		self.state = 'rejected'
 	
