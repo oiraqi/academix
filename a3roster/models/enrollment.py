e94d731ffae4c69ae7d6dd653da969fb56286334
Omar Iraqi <o.iraqi@gmail.com>
2022-06-18 08:13:18 +0100

    updates

--- a3roster/models/enrollment.py
+++ a3roster/models/enrollment.py
@@ -25,7 +25,7 @@ class Enrollment(models.Model):
 		for rec in self:
 			if rec.student_id and rec.section_id:
 				# From a performance perspective, we should start with the cheap is_open check,
-				# then the time conflict check, and only then the prerequisites check.
+				# then the time conflict check, and only then, the prerequisites check.
 				# However, for a more pertinent feedback to the user, we deem it's worth it to start
 				# with the more expensive prerequisites check.
 				self.env['a3roster.enrollment'].check_prerequisites(rec.student_id, rec.section_id.course_id)				
