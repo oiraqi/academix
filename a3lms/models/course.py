fd18fe77604986cff23e8560f9a86225de82d99b
Omar Iraqi <o.iraqi@gmail.com>
2022-06-19 14:36:50 +0100

    updates

--- a3lms/models/course.py
+++ a3lms/models/course.py
@@ -5,9 +5,12 @@ class Course(models.Model):
 	_name = 'a3lms.course'
 	_description = 'LMS Course'
 	_inherit = ['a3.activity']
-	_inherits = {'a3.course': 'course_id', 'a3roster.section': 'section_id'}
 
 	section_id = fields.Many2one(comodel_name='a3roster.section', string='Section', required=True)	
 	name = fields.Char(related='section_id.name')	
-	course_id = fields.Many2one(comodel_name='a3.course', related='section_id.course_id')	
+	course_id = fields.Many2one(comodel_name='a3.course', related='section_id.course_id')
+	instructor_id = fields.Many2one(comodel_name='a3.faculty', related='section_id.instructor_id')
+	timeslot = fields.Char(related='section_id.timeslot')	
+	room_id = fields.Many2one(comodel_name='a3.room', related='section_id.room_id')
+	description = fields.Html(related='course_id.description')	
 	
