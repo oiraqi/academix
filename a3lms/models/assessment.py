5d5f56c548493471f82bdd7914ec87e99614a455
Omar Iraqi <o.iraqi@gmail.com>
2022-06-21 23:03:05 +0100

    updates

--- a3lms/models/assessment.py
+++ a3lms/models/assessment.py
@@ -4,7 +4,6 @@ from odoo import models, fields
 class Assessment(models.Model):
 	_name = 'a3lms.assessment'
 	_description = 'LMS Assessment'
-	_inherit = 'a3.calendarized'
 	_order = 'start_time,module_id'
 
 	name = fields.Char('Name', required=True)
@@ -13,5 +12,4 @@ class Assessment(models.Model):
 	technique_id = fields.Many2one(comodel_name='a3lms.assessment.technique', string='Assessment Technique', required=True)
 	weight = fields.Float(string='Weight (%)')
 	assessment_technique_ids = fields.One2many(comodel_name='a3lms.assessment.technique', related='course_id.assessment_technique_ids')
-	module_ids = fields.One2many(comodel_name='a3lms.module', related='course_id.module_ids')
-				
+	
--- a3quality/models/lms_assessment.py
+++ a3quality/models/lms_assessment.py
@@ -4,5 +4,5 @@ class LmsAssessment(models.Model):
     _inherit = 'a3lms.assessment'
 
     course_ilo_ids = fields.One2many('a3catalog.course.ilo', related='course_id.ilo_ids')
-    ilo_ids = fields.Many2many(comodel_name='a3catalog.course.ilo', string='ILOs')
+    ilo_ids = fields.Many2many(comodel_name='a3catalog.course.ilo', string='Assessed ILOs')
     
