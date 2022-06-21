519584fba037fa90d1c6f655460bbaf5c819ddd7
Omar Iraqi <o.iraqi@gmail.com>
2022-06-21 21:12:48 +0100

    updates

--- a3quality/models/lms_assessment.py
+++ a3quality/models/lms_assessment.py
@@ -3,5 +3,6 @@ from odoo import models, fields
 class LmsAssessment(models.Model):
     _inherit = 'a3lms.assessment'
 
+    course_ilo_ids = fields.One2many('a3catalog.course.ilo', related='course_id.ilo_ids')
     ilo_ids = fields.Many2many(comodel_name='a3catalog.course.ilo', string='ILOs')
     
