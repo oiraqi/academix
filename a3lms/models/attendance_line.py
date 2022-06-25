35b79fc5057f5ffa7ed0e90712cb23eee218c8c4
Omar Iraqi <o.iraqi@gmail.com>
2022-06-25 01:08:12 +0100

    updates

--- a3lms/models/attendance_line.py
+++ a3lms/models/attendance_line.py
@@ -8,7 +8,7 @@ class AttendanceLine(models.Model):
 	student_id = fields.Many2one(comodel_name='a3.student', string='Student', required=True)
 	attendance_id = fields.Many2one(comodel_name='a3lms.attendance', string='Attendance', required=True)
 	section_id = fields.Many2one(comodel_name='a3roster.section', related='attendance_id.section_id', store=True)	
-	state = fields.Selection(string='State', selection=[('present', 'Present'), ('absent', 'Absent'), ('late', 'Late')], default='Present', required=True)
+	state = fields.Selection(string='State', selection=[('present', 'Present'), ('absent', 'Absent'), ('late', 'Late')], default='present', required=True)
 	excused = fields.Boolean(string='Excused', default=False)	
 	program_id = fields.Many2one(comodel_name='a3catalog.program', string='Program', related='student_id.program_id', store=True)
 	school_id = fields.Many2one(comodel_name='a3.school', string='School', related='attendance_id.course_id.section_id.school_id', store=True)
