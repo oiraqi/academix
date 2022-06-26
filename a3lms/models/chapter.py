5c87a0b03b7481fb37af5b5ab7dabe4a1eac90a5
Omar Iraqi <o.iraqi@gmail.com>
2022-06-26 09:19:19 +0100

    updates

--- a3lms/models/chapter.py
+++ a3lms/models/chapter.py
@@ -4,8 +4,10 @@ from odoo import models, fields
 class Chapter(models.Model):
 	_name = 'a3lms.chapter'
 	_description = 'Chapter'
+	_order = "sequence"
 
 	name = fields.Char('Name', required=True)
+	sequence = fields.Integer(string='Sequence')	
 	course_id = fields.Many2one(comodel_name='a3lms.course', string='LMS Course', required=True)
 	module_id = fields.Many2one(comodel_name='a3lms.module', string='Module', required=True)
 	start_date = fields.Date(string='Start Date')
