6434130d4d810a36886aadbc37fb994d0d697b31
Omar Iraqi <o.iraqi@gmail.com>
2022-06-26 11:26:13 +0100

    updates

--- a3lms/models/chapter.py
+++ a3lms/models/chapter.py
@@ -11,5 +11,6 @@ class Chapter(models.Model):
 	course_id = fields.Many2one(comodel_name='a3lms.course', string='LMS Course', required=True)
 	module_id = fields.Many2one(comodel_name='a3lms.module', string='Module', required=True)
 	start_date = fields.Date(string='Start Date')
-	nsessions = fields.Integer(string='Sessions')	
+	nsessions = fields.Integer(string='Sessions')
+	module_ids = fields.One2many(comodel_name='a3lms.module', related='course_id.module_ids')
 		
