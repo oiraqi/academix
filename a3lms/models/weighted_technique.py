d9d478f857da4efd8287a9c9efd87a5d76fec457
Omar Iraqi <o.iraqi@gmail.com>
2022-06-26 13:11:02 +0100

    updates

--- a3lms/models/weighted_technique.py
+++ a3lms/models/weighted_technique.py
@@ -4,12 +4,13 @@ from odoo import models, fields, api
 class WeightedTechnique(models.Model):
 	_name = 'a3lms.weighted.technique'
 	_description = 'Weighted Technique'
+	_sql_constraints = [('technique_ukey', 'unique(technique_id)', 'Assessment techniques: Remove duplicates!')]
 
 	technique_id = fields.Many2one(comodel_name='a3lms.assessment.technique', string='Assessment Technique', required=True)
 	name = fields.Char(related='technique_id.name')
 	sequence = fields.Integer(string='Sequence', required=True)
 	points = fields.Integer(string='Points', compute='_points')
-	percentage = fields.Float(string='%', default=0.0)
+	percentage = fields.Float(string='%', compute='_percentage')
 	course_id = fields.Many2one(comodel_name='a3lms.course', string='LMS Course', required=True)
 	assessment_ids = fields.One2many(comodel_name='a3lms.assessment', inverse_name='technique_id', string='Assessments')	
 
