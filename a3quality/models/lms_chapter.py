da8d4bb4930402e9fd6f4447d52a801d16168f41
Omar Iraqi <o.iraqi@gmail.com>
2022-06-26 13:52:23 +0100

    updates

--- a3quality/models/lms_chapter.py
+++ a3quality/models/lms_chapter.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
 class LmsChapter(models.Model):
-    _inherit = 'a3lms.module'
+    _inherit = 'a3lms.chapter'
 
     course_ilo_ids = fields.One2many('a3catalog.course.ilo', related='course_id.course_id.ilo_ids')
     ilo_ids = fields.Many2many(comodel_name='a3catalog.course.ilo', string='Covered ILOs')
