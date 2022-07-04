e62d370ea320fa1f81805e1c174a2f7cb7cc8ff1
Omar Iraqi <o.iraqi@gmail.com>
2022-07-04 19:34:41 +0100

    updates

--- ixlms/models/mail_channel.py
+++ ixlms/models/mail_channel.py
@@ -1,16 +1,16 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class MailChannel(models.Model):
     _inherit = 'mail.channel'
 
+    cname = fields.Char(string='Channel Name')    
     course_id = fields.Many2one(comodel_name='ixlms.course', string='LMS Course')
 
-    def _resolve_action(self, action_id, domain, context=False):
-        action = self.env['ir.actions.act_window']._for_xml_id(action_id)
-        action['domain'] = domain
-        if context:
-            action['context'] = context		
-        return action
+    @api.onchange('cname', 'course_id')
+    def _cname(self):
+        for rec in self:
+            if rec.course_id and rec.course_id.name and rec.cname:
+                rec.name = rec.course_id.name + rec.cname
 
     def ix_channel_open(self):
         self.ensure_one()
