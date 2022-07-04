f4ae81bc940a74ef01da97e98bda90c1f01e88c8
Omar Iraqi <o.iraqi@gmail.com>
2022-07-04 19:07:44 +0100

    updates

--- ixlms/models/mail_channel.py
+++ ixlms/models/mail_channel.py
@@ -4,4 +4,18 @@ class MailChannel(models.Model):
     _inherit = 'mail.channel'
 
     course_id = fields.Many2one(comodel_name='ixlms.course', string='LMS Course')
-    
+
+    def _resolve_action(self, action_id, domain, context=False):
+        action = self.env['ir.actions.act_window']._for_xml_id(action_id)
+        action['domain'] = domain
+        if context:
+            action['context'] = context		
+        return action
+
+    def ix_channel_open(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'new',
+            'url': f'/chat/{self.id}/{self.uuid}',
+        }
