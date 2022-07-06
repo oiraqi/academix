fc2d03bd07dac30b4329c8d6bf0f2d6f5abecba9
Omar Iraqi <o.iraqi@gmail.com>
2022-07-06 22:18:06 +0100

    updates

--- ixquality/models/assessed_ilo.py
+++ ixquality/models/assessed_ilo.py
@@ -8,7 +8,6 @@ class AssessedIlo(models.Model):
 
 	ilo_id = fields.Many2one(comodel_name='ixcatalog.course.ilo', string='Course ILO', required=True)
 	assessment_line_id = fields.Many2one(comodel_name='ixlms.assessment.line', string='LMS Assessment Line', required=True)
-	acquisition_level_id = fields.Many2one(comodel_name='ixquality.acquisition.level', string='Acquisition Level')
-	assessment_ilo_ids = fields.Many2many(comodel_name='ixcatalog.course.ilo', related='assessment')
+	acquisition_level_id = fields.Many2one(comodel_name='ixquality.acquisition.level', string='Acquisition Level')	
 	ilo_idx_description = fields.Char(related='ilo_id.idx_description')
 		
