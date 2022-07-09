2c171e707c708cd19856c35d516b18ef1d83cd88
Omar Iraqi <o.iraqi@gmail.com>
2022-07-09 22:27:17 +0100

    updates

--- ixlms/models/chapter.py
+++ ixlms/models/chapter.py
@@ -12,4 +12,12 @@ class Chapter(models.Model):
 	course_id = fields.Many2one(comodel_name='ixlms.course', related='module_id.course_id', store=True)
 	start_date = fields.Date(string='Start Date')
 	nsessions = fields.Integer(string='Sessions')
+	resource_ids = fields.One2many(comodel_name='ixlms.resource', inverse_name='chapter_id', string='Resources')
+	nresources = fields.Integer(string='Resources', compute='_nresources')
+
+	def _nresources(self):
+		for rec in self:
+			rec.nresources = len(rec.resource_ids)
+	
+	
 		
