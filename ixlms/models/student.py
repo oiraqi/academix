3dfb0e3c6538c681ca3be49f9a45eb489531a73d
Omar Iraqi <o.iraqi@gmail.com>
2022-07-09 10:34:45 +0100

    updates

--- ixlms/models/student.py
+++ ixlms/models/student.py
@@ -30,7 +30,7 @@ class Student(models.Model):
     attendance_line_absent_ids = fields.One2many(comodel_name='ixlms.attendance.line', compute='_attendance_line_absent_ids', string='Missed Classes')
     earned_sch = fields.Integer(compute='_sch', string='Earned Credits')
     remaining_sch = fields.Integer(compute='_sch', string='Remaining Credits')
-    progress_sch = fields.Float(string='Progress', compute='_sch')
+    progress = fields.Float(string='Progress', compute='_sch')
     
 
     def _sch(self):
