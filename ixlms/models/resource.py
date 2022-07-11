96f90a2befeff534703b7b3a6a3aa9d93f7ad8a6
Omar Iraqi <o.iraqi@gmail.com>
2022-07-11 18:09:05 +0100

    updates

--- ixlms/models/resource.py
+++ ixlms/models/resource.py
@@ -9,7 +9,7 @@ class Resource(models.Model):
 	chapter_id = fields.Many2one(comodel_name='ixlms.chapter', string='Chapter', required=True)
 	module_id = fields.Many2one(comodel_name='ixlms.module', string='Module', required=True)
 	course_id = fields.Many2one(comodel_name='ixlms.course', string='LMS Course', required=True)
-	document_id = fields.Many2one(comodel_name='ixdms.document', string='Document', required=True)
+	document_id = fields.Many2one(comodel_name='ixdms.node', string='Document', required=True)
 	file = fields.Binary(related='document_id.file')
 	url = fields.Char(related='document_id.url')	
 	
