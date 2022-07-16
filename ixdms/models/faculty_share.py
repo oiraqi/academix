3d6afa15d71be8ae66da239c05621fea1f1ff6f3
Omar Iraqi <o.iraqi@gmail.com>
2022-07-16 17:38:31 +0100

    updates

--- ixdms/models/faculty_share.py
+++ ixdms/models/faculty_share.py
@@ -5,8 +5,8 @@ class FacultyShare(models.Model):
 	_name = 'ixdms.faculty.share'
 	_description = 'Faculty Share'
 	_inherit = 'ix.school.owned'
-	_sql_constraints = [('share_ukey', 'unique(node_id, school_id, discipline_id)', 'Duplicate shares!')]
+	_sql_constraints = [('share_ukey', 'unique(share_id, school_id, discipline_id)', 'Duplicate shares!')]
 
-	node_id = fields.Many2one(comodel_name='ixdms.node', string='Node', required=True)
+	share_id = fields.Many2one(comodel_name='ixdms.node', string='Share', required=True)
 	discipline_id = fields.Many2one(comodel_name='ix.discipline', string='Discipline')
 	
--- ixdms/models/student_share.py
+++ ixdms/models/student_share.py
@@ -5,8 +5,8 @@ class StudentShare(models.Model):
 	_name = 'ixdms.student.share'
 	_description = 'Student Share'
 	_inherit = 'ix.school.owned'
-	_sql_constraints = [('share_ukey', 'unique(node_id, school_id, program_id)', 'Duplicate shares!')]
+	_sql_constraints = [('share_ukey', 'unique(share_id, school_id, program_id)', 'Duplicate shares!')]
 
-	node_id = fields.Many2one(comodel_name='ixdms.node', string='Node', required=True)
+	share_id = fields.Many2one(comodel_name='ixdms.node', string='Share', required=True)
 	program_id = fields.Many2one(comodel_name='ixcatalog.program', string='Program')
 		
