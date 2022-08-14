5f6de9f913d08fc93c9abbc629c44e0277b6e48e
Omar Iraqi <o.iraqi@gmail.com>
2022-08-14 17:32:41 +0100

    updates

--- ixquality/models/lms_course.py
+++ ixquality/models/lms_course.py
@@ -28,5 +28,5 @@ class LmsCourse(models.Model):
 
     ilo_program_ids = fields.One2many('ixquality.lms.course.ilo.program', inverse_name='course_id')
     acquisition_level = fields.Selection(string='Acquisition Level', selection=[
-		('0', 'Not acquired'), ('3', '60%+'), ('4', '80%+'), ('5', 'Fully Acquired')], default='4')
+		('3', '60%+'), ('4', '80%+'), ('5', 'Fully Acquired')], default='4', required=True)
     
