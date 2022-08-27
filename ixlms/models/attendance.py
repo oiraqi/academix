41ba2638d27ec011a86a653b19641f42ffe9eddc
Omar Iraqi <o.iraqi@gmail.com>
2022-08-27 11:18:22 +0100

    updates

--- ixlms/models/attendance.py
+++ ixlms/models/attendance.py
@@ -32,10 +32,10 @@ class Attendance(models.Model):
 
 	name = fields.Char('Name', compute='_set_name')
 	lms_course_id = fields.Many2one(comodel_name='ixlms.course', string='Course', required=True)	
-	section_id = fields.Many2one(comodel_name='ixroster.section', related='course_id.section_id', store=True)
-	term_id = fields.Many2one(comodel_name='ix.term', related='course_id.section_id.term_id', store=True)
-	school_id = fields.Many2one(comodel_name='ix.school', related='course_id.section_id.school_id', store=True)
-	instructor_id = fields.Many2one(comodel_name='ix.faculty', related='course_id.section_id.instructor_id', store=True)
+	section_id = fields.Many2one(comodel_name='ixroster.section', related='lms_course_id.section_id', store=True)
+	term_id = fields.Many2one(comodel_name='ix.term', related='lms_course_id.section_id.term_id', store=True)
+	school_id = fields.Many2one(comodel_name='ix.school', related='lms_course_id.section_id.school_id', store=True)
+	instructor_id = fields.Many2one(comodel_name='ix.faculty', related='lms_course_id.section_id.instructor_id', store=True)
 	day = fields.Date(string='Date', required=True, default= lambda self: fields.Date.today())
 	attendance_line_ids = fields.One2many(comodel_name='ixlms.attendance.line', inverse_name='attendance_id', string='Attendance Lines')
 	npresent = fields.Integer(string='Present', compute='_stats')
@@ -70,16 +70,16 @@ class Attendance(models.Model):
 	
 	def _set_name(self):
 		for rec in self:
-			if rec.course_id and rec.day:
-				rec.name = 'Attendance / ' + rec.course_id.name + ' / ' + fields.Date.to_string(rec.day)
+			if rec.lms_course_id and rec.day:
+				rec.name = 'Attendance / ' + rec.lms_course_id.name + ' / ' + fields.Date.to_string(rec.day)
 			else:
 				rec.name = ''
 
-	@api.onchange('course_id', 'day')
-	def _onchange_course_id(self):
+	@api.onchange('lms_course_id', 'day')
+	def _onchange_lms_course_id(self):
 		for rec in self:
-			if rec.day and rec.course_id and rec.course_id.student_ids and not rec.attendance_line_ids:
-				for student in rec.course_id.student_ids:
+			if rec.day and rec.lms_course_id and rec.lms_course_id.student_ids and not rec.attendance_line_ids:
+				for student in rec.lms_course_id.student_ids:
 					rec.attendance_line_ids += self.env['ixlms.attendance.line'].new({
 						'student_id': student.id,
 						'attendance_id': rec.id,
