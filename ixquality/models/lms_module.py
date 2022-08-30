41df593b70dd45ce4953ef0a0da0532603aed6c6
Omar Iraqi <o.iraqi@gmail.com>
2022-08-30 03:00:55 +0100

    updates

--- ixquality/models/lms_module.py
+++ ixquality/models/lms_module.py
@@ -26,18 +26,18 @@ from odoo import models, fields, api
 class LmsModule(models.Model):
     _inherit = 'ixlms.module'
 
-    ilo_ids = fields.Many2many(comodel_name='ixcatalog.course.ilo', compute='_ilo_ids', string='Covered ILOs')
+    lms_course_ilo_ids = fields.Many2many(comodel_name='ixlms.course.ilo', compute='_ilo_ids', string='Covered ILOs')
 
     @api.onchange('chapter_ids')
     def _ilo_ids(self):
         for rec in self:
             ilos = []
             for chapter in rec.chapter_ids:
-                for ilo in chapter.ilo_ids:
+                for ilo in chapter.lms_course_ilo_ids:
                     if ilo.id not in ilos:
                         ilos.append(ilo.id)
             if len(ilos) > 0:
-                rec.ilo_ids = ilos                
+                rec.lms_course_ilo_ids = ilos                
             else:
-                rec.ilo_ids = False
+                rec.lms_course_ilo_ids = False
     
