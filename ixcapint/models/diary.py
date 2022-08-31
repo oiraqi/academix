f9c7a8d206f2add7d919d760ac841007a11f4011
Omar Iraqi <o.iraqi@gmail.com>
2022-08-31 16:19:02 +0100

    updates

--- ixcapint/models/diary.py
+++ ixcapint/models/diary.py
@@ -33,5 +33,5 @@ class Diary(models.Model):
 
 	def open_diary(self):
 		self.ensure_one()		
-		return self._expand_to('ixcapint.action_diary', False, {'default_project_id': self.project_id.id}, self.id)
+		return self._expand_to('ixcapint.action_diary', [('id', '=', self.id)], {'default_project_id': self.project_id.id}, self.id)
 
