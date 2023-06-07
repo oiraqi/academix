31aa65d649d9777d73fb0f72f2e757c5e6e2da87
Omar IRAQI <o.iraqi@gmail.com>
2023-06-07 02:37:09 +0100

    updates

--- ixquality/models/lms_course.py
+++ ixquality/models/lms_course.py
@@ -26,21 +26,6 @@ from odoo import models, fields, api
 class LmsCourse(models.Model):
     _inherit = 'ixlms.course'
 
-    @api.model
-    def create(self, vals):
-        lms_course = super(LmsCourse, self).create(vals)
-        for lms_course_ilo in lms_course.lms_course_ilo_ids:
-            for program in lms_course.program_ids:                
-                ilo_so_ids = self.env['ixquality.course.ilo.so'].search([('course_id', '=', lms_course.course_id.id), ('program_id', '=', program.id), ('ilo_id.sequence', '=', lms_course_ilo.sequence)])
-                for ilo_so in ilo_so_ids:
-                    self.env['ixquality.lms.course.ilo.so'].create({
-                        'lms_course_ilo_id': lms_course_ilo.id,
-                        'so_id': ilo_so.so_id.id,
-                        'course_program_id': ilo_so.course_program_id.id,
-                        'level': ilo_so.level,
-                    })
-        return lms_course
-
     ilo_program_ids = fields.One2many('ixquality.lms.course.ilo.program', inverse_name='lms_course_id', groups="ix.group_faculty,ix.group_coordinator,ix.group_vpaa")
     acquisition_level = fields.Selection(string='Targetted Acquisition Level (TAL)', selection=[
 		('1', '60%'), ('2', '70%'), ('3', '80%'), ('4', '90%'), ('5', 'Fully Acquired')], default='3', required=True)
