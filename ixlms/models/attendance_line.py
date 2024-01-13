101344f8a9e2361d23d469b027d6e81e2586e21e
Omar IRAQI <o.iraqi@gmail.com>
2024-01-13 12:52:08 +0100

    update

--- ixlms/models/attendance_line.py
+++ ixlms/models/attendance_line.py
@@ -35,6 +35,6 @@ class AttendanceLine(models.Model):
 	section_id = fields.Many2one(comodel_name='ixroster.section', related='attendance_id.section_id', store=True)	
 	state = fields.Selection(string='State', selection=[('present', 'Present'), ('absent', 'Absent - Unexcused'), ('late', 'Late'), ('absentx', 'Absent - Excused')], default='present', required=True)
 	program_id = fields.Many2one(comodel_name='ixcatalog.program', string='Program', related='student_id.program_id', store=True)
-	school_id = fields.Many2one(comodel_name='ix.school', string='School', related='attendance_id.lms_course_id.section_id.school_id', store=True)
+	school_id = fields.Many2one(comodel_name='ix.school', string='School', related='attendance_id.section_id.school_id', store=True)
 	day = fields.Date(related='attendance_id.day', store=True)
 		
