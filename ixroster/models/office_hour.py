7b073faa2372846302ccdfa29ac1973f6694bbb6
Omar IRAQI <o.iraqi@gmail.com>
2024-09-06 06:35:19 +0100

    updates

--- ixroster/models/office_hour.py
+++ ixroster/models/office_hour.py
@@ -1,10 +1,16 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class OfficeHour(models.Model):
 	_name = 'ixroster.office.hour'
 	_description = 'Office Hour'
 	_inherit = ['ix.faculty.owned', 'ixroster.scheduled']
+	_mapping = {'f2f': 'Face To Face', 'online': 'Online'}
 
-	name = fields.Char(related='timeslot')
+	def _compute_name(self):
+		for rec in self:
+			rec.name = rec.timeslot.name + ' (' + OfficeHour._mapping[rec.type] + ')'
+	
+	name = fields.Char(compute='_compute_name')
+	type = fields.Selection(string='Type', selection=[('f2f', 'Face To Face'), ('online', 'Online')], default='f2f')
 	
